68e550f0fda98ef2defcb9b530016a7092fbd8c2
Great Ogheneochuko <greatochuko123@gmail.com>
2025-01-31 16:58:24 +0100

    refactor authentication routes by extracting logic into controller functions

--- server/src/routes/authRoute.js
+++ server/src/routes/authRoute.js
@@ -2,105 +2,12 @@ import bcrypt from "bcryptjs";
 import { Router } from "express";
 import User from "../models/User.js";
 import jwt from "jsonwebtoken";
+import { login, register } from "../controllers/authController.js";
 
 const router = Router();
 
-router.post("/register", async (req, res) => {
-  try {
-    // Get the user input
-    const { firstName, lastName, email, password } = req.body;
-    if (!firstName || !lastName || !email || !password) {
-      return res
-        .status(400)
-        .json({ message: "Please provide the name, email and password" });
-    }
+router.post("/register", register);
 
-    // Check if the email is valid
-    const emailRegex = /\S+@\S+\.\S+/;
-    if (!emailRegex.test(email)) {
-      return res.status(400).json({ message: "Invalid email" });
-    }
-
-    // Check if the user already exists
-    const userExists = await User.findOne({ email });
-    if (userExists) {
-      return res
-        .status(400)
-        .json({ message: "User with this email already exists" });
-    }
-
-    // Encrypt the password
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-
-    // Save the user to the database
-    const createdUser = await User.create({
-      firstName,
-      lastName,
-      email,
-      password: hashedPassword,
-    });
-
-    const newUser = await User.findById(createdUser._id).select("-password");
-
-    // Create a token
-    const token = jwt.sign({ id: createdUser._id }, process.env.JWT_SECRET, {
-      expiresIn: "30d",
-    });
-
-    res.cookie("token", token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-    });
-
-    res
-      .status(200)
-      .json({ message: "User created successfully", data: newUser });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-router.post("/login", async (req, res) => {
-  try {
-    // Get the user input
-    const { email, password } = req.body;
-    if (!email || !password) {
-      return res
-        .status(400)
-        .json({ message: "Please provide the email and password" });
-    }
-
-    // Check if the user exists
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res
-        .status(400)
-        .json({ message: "Invalid email and password combination" });
-    }
-
-    // Check if the password is correct
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-    if (!isPasswordValid) {
-      return res
-        .status(400)
-        .json({ message: "Invalid email and password combination" });
-    }
-
-    // Create a token
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-      expiresIn: "30d",
-    });
-
-    res.cookie("token", token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-    });
-
-    res.json({ message: "Login successful", data: user });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
+router.post("/login", login);
 
 export default router;
