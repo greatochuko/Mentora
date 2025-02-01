c1d711fddb3b9d4c4d332546d066fe4ca2c9afab
Great Ogheneochuko <greatochuko123@gmail.com>
2025-02-01 13:58:32 +0100

    configure CORS middleware for enhanced security

--- server/src/app.js
+++ server/src/app.js
@@ -8,8 +8,15 @@ dotenv.config();
 
 const app = express();
 
+const corsOptions = {
+  origin: ["http://localhost:5173"],
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
 // Middleware
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
