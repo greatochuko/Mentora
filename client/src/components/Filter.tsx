cb524f2e77f5de518ed5dcac278bf4c76a1fdd45
Great Ogheneochuko <greatochuko123@gmail.com>
2025-02-02 16:32:21 +0100

    Add filter functionality with category selection and clear/apply buttons

--- client/src/components/Filter.tsx
+++ client/src/components/Filter.tsx
@@ -1,13 +1,41 @@
 import { useState } from "react";
 import { FaStar } from "react-icons/fa";
 import { categories } from "../lib/data";
+import { useSearchParams } from "react-router-dom";
 
 export default function Filter({ showFilter }: { showFilter: boolean }) {
-  const [rating, setRating] = useState("all");
+  const [rating, setRating] = useState<string>();
+  const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  function toggleSelectedCategories(category: string) {
+    if (selectedCategories.includes(category)) {
+      setSelectedCategories((prev) => prev.filter((cat) => cat !== category));
+    } else {
+      setSelectedCategories((prev) => [...prev, category]);
+    }
+  }
+
+  function clearFilters() {
+    setRating(undefined);
+    setSelectedCategories([]);
+    searchParams.delete("rating");
+    searchParams.delete("categories");
+    setSearchParams(searchParams);
+  }
+
+  function applyFilters() {
+    rating ? searchParams.set("rating", rating) : searchParams.delete("rating");
+    selectedCategories.length
+      ? searchParams.set("categories", selectedCategories.toString())
+      : searchParams.delete("categories");
+    setSearchParams(searchParams);
+  }
 
   return (
     <div
-      className={`h-fit rounded-md border-zinc-300 duration-300 ease-linear ${showFilter ? "mr-4 w-72 border" : "w-0"}`}
+      className={`h-fit overflow-hidden rounded-md border-zinc-300 whitespace-nowrap duration-300 ease-linear ${showFilter ? "mr-4 w-72 border" : "w-0"}`}
     >
       <h3 className="border-b border-zinc-300 p-2 text-center text-lg font-medium">
         Filter
@@ -21,8 +49,8 @@ export default function Filter({ showFilter }: { showFilter: boolean }) {
                 type="radio"
                 name="rating"
                 id="all"
-                checked={rating === "all"}
-                onChange={() => setRating("all")}
+                checked={!rating}
+                onChange={() => setRating(undefined)}
                 className="accent-blue-500"
               />
               <FaStar className="h-4 w-4 fill-amber-500" />
@@ -60,12 +88,29 @@ export default function Filter({ showFilter }: { showFilter: boolean }) {
                   type="checkbox"
                   name={category.slug}
                   id={category.slug}
+                  checked={selectedCategories.includes(category.slug)}
+                  onChange={() => toggleSelectedCategories(category.slug)}
+                  className="accent-blue-500"
                 />
                 {category.name}
               </label>
             </li>
           ))}
         </ul>
+      </div>
+      <div className="flex gap-4 p-4">
+        <button
+          onClick={clearFilters}
+          className="flex-1 rounded-md bg-rose-500 px-4 py-2 font-medium text-white duration-300 hover:bg-rose-600"
+        >
+          Clear Filters
+        </button>
+        <button
+          onClick={applyFilters}
+          className="flex-1 rounded-md bg-blue-500 px-4 py-2 font-medium text-white duration-300 hover:bg-blue-600"
+        >
+          Apply Filters
+        </button>
       </div>
     </div>
   );
