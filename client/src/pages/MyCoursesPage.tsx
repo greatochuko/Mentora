c20c592fd828c4e49013683ba34a45d0d21dda54
Great Ogheneochuko <greatochuko123@gmail.com>
2025-02-08 15:34:08 +0100

    Fetch user session data on MyCoursesPage load and update user context

--- client/src/pages/MyCoursesPage.tsx
+++ client/src/pages/MyCoursesPage.tsx
@@ -2,11 +2,26 @@ import { Navigate } from "react-router-dom";
 import LoadingPage from "../components/LoadingPage";
 import { useUserContext } from "../context/userContext";
 import MyCourseCard from "../components/MyCourseCard";
+import { useEffect } from "react";
+import useFetch from "../hooks/useFetch";
 
 export default function MyCoursesPage() {
-  const { user, loadingSession } = useUserContext();
+  const { user, loadingSession, updateUser } = useUserContext();
 
   if (loadingSession) return <LoadingPage />;
+
+  const { fetchData } = useFetch({
+    url: "/auth/session",
+    startLoading: true,
+    initialData: [],
+    onSuccess(result) {
+      updateUser(result.data);
+    },
+  });
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   if (!user) return <Navigate to={"/login"} replace />;
 
@@ -14,9 +29,10 @@ export default function MyCoursesPage() {
     <main className="mx-auto flex w-[90%] max-w-7xl flex-1 flex-col gap-8 py-8">
       <h1 className="text-lg font-medium sm:text-xl">My Courses</h1>
       <div className="grid grid-cols-[repeat(auto-fill,_minmax(240px,_1fr))] gap-4">
-        {user?.paidCourses.map((course) => (
-          <MyCourseCard key={course._id} course={course} />
-        ))}
+        {user?.paidCourses &&
+          user.paidCourses.map((course) => (
+            <MyCourseCard key={course._id} course={course} />
+          ))}
       </div>
     </main>
   );
