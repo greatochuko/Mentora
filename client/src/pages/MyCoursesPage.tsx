bf332ab326b0c5e3b31f855296dac29ba55da106
Great Ogheneochuko <greatochuko123@gmail.com>
2025-02-08 15:24:03 +0100

    Fix optional chaining for user paidCourses in MyCoursesPage component

--- client/src/pages/MyCoursesPage.tsx
+++ client/src/pages/MyCoursesPage.tsx
@@ -14,7 +14,7 @@ export default function MyCoursesPage() {
     <main className="mx-auto flex w-[90%] max-w-7xl flex-1 flex-col gap-8 py-8">
       <h1 className="text-lg font-medium sm:text-xl">My Courses</h1>
       <div className="grid grid-cols-[repeat(auto-fill,_minmax(240px,_1fr))] gap-4">
-        {user!.paidCourses.map((course) => (
+        {user?.paidCourses.map((course) => (
           <MyCourseCard key={course._id} course={course} />
         ))}
       </div>
